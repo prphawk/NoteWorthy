a2f5c04e7c541d39438289ad06e06d31aa3aaf9f
Mayra <mayra.cademartori@gmail.com>
2021-03-29 16:54:26 -0300

    Refactor

--- src/noteworthy/Music.java
+++ src/noteworthy/Music.java
@@ -52,67 +52,75 @@ public class Music {
         volume = VOLUME_DEFAULT;
         noteString = Note.PAUSE;                                                //caso a operação seja repetir ultima nota sem nenhuma nota ter sido tocada, emite pausa -> "R "
     }
-    
+
     public Pattern build(){                                                     //constroi uma string personalizada de acordo com os comandos do jfugue
-       
+
         StringBuilder musicString = new StringBuilder();                        //para construir a pattern final a partir de uma string formada com StringBuilder
-        
+
         for (int i = 0; i < text.length(); i++) {                               //cada char do texto
-            
+
             char ch = text.charAt(i);
-            
-            switch(ch){
-                
-                case 'a': musicString.append(setNoteTo(Note.LA)); break;
-                case 'b': musicString.append(setNoteTo(Note.SI)); break;
-                case 'c': musicString.append(setNoteTo(Note.DO)); break;
-                case 'd': musicString.append(setNoteTo(Note.RE)); break;
-                case 'e': musicString.append(setNoteTo(Note.MI)); break;
-                case 'f': musicString.append(setNoteTo(Note.FA)); break;
-                case 'g': musicString.append(setNoteTo(Note.SOL)); break;
-                case ' ': 
+
+            appendNotes(ch, musicString);
+
+            appendInstruments(ch, musicString);
+
+            switch(ch) {
+
+                case ' ':
                 case 'i':
                 case 'o':
                 case 'u': musicString.append(noteString); break;                //printa na string final a ultima nota tocada
                 case ';': musicString.append(Note.PAUSE); break;
-                
+
                 case '.':
-                case '?': octave = OCTAVE_DEFAULT; volume = VOLUME_DEFAULT; break;                       //volta a oitava e volume default
-                
+                case '?': octave = OCTAVE_DEFAULT; volume = VOLUME_DEFAULT; break;//volta a oitava e volume default
+
                 case '+': musicString.append(setVolumeTo(volume+VOLUME_STEP)); break;
                 case '-': musicString.append(setVolumeTo(volume-VOLUME_STEP)); break;
-                
+
                 case 'T':
                     if(text.charAt(i+1) == '+') musicString.append(setBPMTo(bpm+50));
                     if(text.charAt(i+1) == '-') musicString.append(setBPMTo(bpm-50));
                     i++; break;
-                
-                case 'O': 
+
+                case 'O':
                     if(text.charAt(i+1) == '+' && octave < 9) octave++;         //de 0 a 9 sempre
                     if(text.charAt(i+1) == '-' && octave > 0) octave--;
                     i++; break;
-                   
-                case '\n': 
-                    Random rand = new Random();
-                    musicString.append(setInstrumentTo(rand.nextInt(MAX_LIMIT))); break; 
-                   
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9': 
-                    int chValue = Character.getNumericValue(ch);
-                    musicString.append(setInstrumentTo(instrument+chValue)); break;
-                    
-                //default: musicString.append(noteString); break;
 
+                case '\n':
+                    Random rand = new Random();
+                    musicString.append(setInstrumentTo(rand.nextInt(MAX_LIMIT))); break;
             }
         }
         return new Pattern(musicString.toString());                             //retorna nova Pattern construida
+    }
+
+    private void appendNotes(char ch, StringBuilder musicString) {
+
+        if((int) ch >= 'a' && (int) ch <= 'g') {
+
+            String note = Note.LA;
+
+            switch (ch) {
+                case 'a': break;
+                case 'b': note = Note.SI; break;
+                case 'c': note = Note.DO; break;
+                case 'd': note = Note.RE; break;
+                case 'e': note = Note.MI; break;
+                case 'f': note = Note.FA; break;
+                case 'g': note = Note.SOL; break;
+            }
+
+            musicString.append(setNoteTo(note));
+        }
+    }
+
+    private void appendInstruments(int ascii, StringBuilder musicString) {
+        if(ascii >= '1' && ascii <= '9') {
+            musicString.append(setInstrumentTo(instrument+ ascii));
+        }
     }
 
     private String setBPMTo(int value){
